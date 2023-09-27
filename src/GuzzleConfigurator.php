a0075cc8abdf3276468f984a98540beff811ba2e
aaron <aaron.romero@cedesa.es>
2023-09-27 13:31:19 +0200

    initial

--- src/GuzzleConfigurator.php
+++ src/GuzzleConfigurator.php
@@ -2,7 +2,39 @@
 
 namespace Aaronr0207\MailServiceHelper;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+
 class GuzzleConfigurator
 {
+    public function configure(array $custom_options = [])
+    {
+        $handlerStack = HandlerStack::create();
+        $handlerStack->push(Middleware::retry(function($retry, $request, $response, $exception) {
+            // Si la respuesta es 429, entonces reintenta
+            if ($response && $response->getStatusCode() === 429) {
+                sleep( env('MAIL_SERVICE_URI',3)); // esperar X segundos
+                return true;
+            }
+            return false;
+        }));
 
+        $default_options = [
+            'base_uri' => env('MAIL_SERVICE_URI','localhost'),
+            'headers' => [
+                'X-API-KEY' => ENV('MAIL_SERVICE_API_KEY','******'),
+                'Content-Type' => 'multipart/form-data'
+            ],
+            'handler' => $handlerStack
+        ];
+
+
+
+        // Aquí puedes agregar cualquier lógica de configuración personalizada.
+        // Por ejemplo, podrías agregar opciones predeterminadas o leer
+        // configuración de un archivo.
+        $options = array_merge($default_options,$custom_options);
+        return new Client($options);
+    }
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,8 +1,56 @@
 <?php
 
-if (! function_exists('mi_helper')) {
-    function mi_helper()
+if (! function_exists('send_mail')) {
+    function send_mail($emisor,$asunto,$vista,$parametros = [],$receptores,$copia = [],$copia_oculta = [])
     {
-        // ...
+        $configurator = new \Aaronr0207\MailServiceHelper\GuzzleConfigurator();
+        $cliente = $configurator->configure();
+
+        // Ahora puedes usar $client para hacer solicitudes HTTP al microservicio.
+        $datos = [];
+
+
+        $datos[] = [
+            'name' => 'tipo',
+            'contents' => 'email'
+        ];
+        $datos[] = [
+            'name' => 'subject',
+            'contents' => $asunto
+        ];
+        $datos[] = [
+            'name' => 'body',
+            'contents' => view($vista, $parametros)->render()
+        ];
+        $datos[] = [
+            'name' => 'sender',
+            'contents' => $emisor
+        ];
+        $datos[] = [
+            'name' => 'recipients',
+            'contents' => json_encode($receptores)
+        ];
+        $datos[] = [
+            'name' => 'cc',
+            'contents' => json_encode($copia)
+        ];
+        $datos[] = [
+            'name' => 'bcc',
+            'contents' => json_encode($copia_oculta)
+        ];
+        $datos[] = [
+            'name' => 'api_caller',
+            'contents' => 'ourense'
+        ];
+        $datos[] = [
+            'name' => 'instance_caller',
+            'contents' => 'ourense'
+        ];
+
+
+
+        return $cliente->post('/api/v1/incoming-mail', [
+            'multipart' => $datos
+        ]);
     }
 }
