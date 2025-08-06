3f7518fe5d31b7af8b3cfc0fb44c1eb614919cd0
Jorge Perez <jperez@cedesa.com>
2025-08-06 09:30:20 +0200

    Add LapsoMail transport integration and configuration support

--- src/MailServiceHelperServiceProvider.php
+++ src/MailServiceHelperServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Aaronr0207\MailServiceHelper;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Mail\MailManager;
 
 class MailServiceHelperServiceProvider extends ServiceProvider
 {
@@ -10,11 +11,20 @@ class MailServiceHelperServiceProvider extends ServiceProvider
     public function register()
     {
         require_once __DIR__ . '/helpers.php';
+        
+        // Merge mail configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/lapso-mail.php', 'mail.mailers.lapso-mail-service');
     }
-
 
     public function boot()
     {
+        // Register mail driver
+        $this->app->extend('mail.manager', function (MailManager $mailManager) {
+            $mailManager->extend('lapso-mail-service', function (array $config = []) {
+                return new LapsoMailTransport();
+            });
+            return $mailManager;
+        });
     }
 
 }
